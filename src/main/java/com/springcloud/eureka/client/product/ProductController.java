203d687fd5330afc835a8de497388250a936308d
zapzookj <zapzook@naver.com>
2025-02-10 12:17:16 +0900

    config 서버 <-> product 서버 yml 설정 연동 및 리프레시 설정

--- src/main/java/com/springcloud/eureka/client/product/ProductController.java
+++ src/main/java/com/springcloud/eureka/client/product/ProductController.java
@@ -1,18 +1,22 @@
 package com.springcloud.eureka.client.product;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class ProductController {
 
     @Value("${server.port}")
     private String serverPort;
 
-    @GetMapping("/product/{id}")
-    public String getProduct(@PathVariable("id") String id) {
-        return "Product " + id + "info!!  From port : " + serverPort;
+    @Value("${message}")
+    private String message;
+
+    @GetMapping("/product")
+    public String getProduct() {
+        return "Product info!!  From port : " + serverPort + "and message: " + message;
     }
 }
